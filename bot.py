232d81638f96833e3ce0f6f72d161ff9e4d630da
booydar <ayd98@mail.ru>
2025-05-12 23:55:40 +0300

    feat: refactor messaging

--- bot.py
+++ bot.py
@@ -34,7 +34,9 @@ class NewsBot(AsyncTeleBot):
         self.last_msg_id = None
         self.channel_ratings = None
         self.start_timer(update_interval)
+        self.load_ratings()
         self.load_messages()
+        self.current_messages = dict()
     
     def load_messages(self):
         print(f"{datetime.datetime.now().strftime('%H:%M %d.%m.%Y')}\nLoading all messages")
@@ -42,74 +44,45 @@ class NewsBot(AsyncTeleBot):
         with open(MESSAGES_PATH, 'r') as f:
             messages = json.load(f)
 
+        rated_ids = {str(r['id']) + '-' + str(r['channel_id']) for r in self.ratings}
+        messages = list(filter(lambda msg: str(msg['id']) + '-' + str(msg['channel_id']) not in rated_ids, messages))
+
         if self.filter_by_group not in {'all', None}:
             messages = list(filter(lambda msg: handler.get_group_name(msg['channel_id']) == self.filter_by_group, messages))
+        self.messages = self.sort(messages)
+        print(f"Found {len(self.messages)} total messages")
 
+    def load_ratings(self):
+        self.ratings = []
         if os.path.exists(RATINGS_PATH):
             with open(RATINGS_PATH, 'r') as f:
                 ratings = json.load(f)
-            rated_ids = {str(r['id']) + '-' + str(r['channel_id']) for r in ratings}
-            messages = list(filter(lambda msg: str(msg['id']) + '-' + str(msg['channel_id']) not in rated_ids, messages))
 
             self.ratings = ratings
             self.channel_ratings = get_channel_ratings(ratings)
-            self.messages = self.sort(messages)
-            print(f"Found {len(messages)} total messages and {len(self.ratings)} ratings")
-            if self.messages[0] == self.selected_message:
-                self.messages = self.messages[1:]
-            return
-            
-        self.messages = messages
-        self.ratings = []
+            print(f"Found {len(self.ratings)} ratings")
+        else:
+            print("Ratings file not found. Starting with no ratings.")
 
     def filter_messages(self, filter_by_group):
-        with open(MESSAGES_PATH, 'r') as f:
-            messages = json.load(f)
+        self.filter_by_group = filter_by_group
+        self.load_messages()
+        self.load_ratings()
 
-        if filter_by_group not in {'all', None}:
-            messages = list(filter(lambda msg: handler.get_group_name(msg['channel_id']) == self.filter_by_group, messages))
-
-        if os.path.exists(RATINGS_PATH):
-            with open(RATINGS_PATH, 'r') as f:
-                ratings = json.load(f)
-            rated_ids = {str(r['id']) + '-' + str(r['channel_id']) for r in ratings}
-            messages = list(filter(lambda msg: str(msg['id']) + '-' + str(msg['channel_id']) not in rated_ids, messages))
-
-            self.ratings = ratings
-            self.channel_ratings = get_channel_ratings(ratings)
-            self.messages = self.sort(messages)
-            print(f"Found {len(messages)} total messages and {len(self.ratings)} ratings")
+    def get_new_messages(self, n=3):
+        new_messages = self.messages[:n]
+        self.messages = self.messages[n:]
+        return new_messages
     
-    def remove_message(self, message):
-        with open(MESSAGES_PATH, 'r') as f:
-            messages = json.load(f)
-
-        prevoius_len = len(messages)
-        messages = list(filter(lambda msg: (msg['id'] != message['id']) or \
-                                      (msg['channel_id'] != message['channel_id']), messages))
-        print(f"Removing {prevoius_len - len(messages)} messages")
-        with open(MESSAGES_PATH, 'w') as f:
-            json.dump(messages, f, ensure_ascii=False)
-        self.selected_message = None
-
-    def get_message(self):
-        if self.selected_message is not None:
-            return self.selected_message
-        else:
-            message = self.messages[0]
-            self.messages = self.messages[1:]
-            self.selected_message = message
-            return message
-    
-    def set_rating(self, rating, is_advertisement=False):
-        message = dict(**self.selected_message)
+    def rate(self, message, rating, is_advertisement=False):
+        print(message)
+        message = dict(message)
         message.pop('message')
         message['rating'] = rating
         message['is_advertisement'] = is_advertisement
         if hasattr(self, "channel_ratings"):
-            message['ranking_method'] = "channel_ratings_v2"
+            message['ranking_method'] = "channel_ratings_v3"
         self.ratings.append(message)
-        self.selected_message = None
 
         with open(RATINGS_PATH, 'w') as f:
             json.dump(self.ratings, f, ensure_ascii=False)
@@ -144,12 +117,12 @@ chat_id = int(api_token.split(":")[0])
 bot = NewsBot(api_token, int(update_interval))
 handler = MessageHandler()
 
-def rate_markup():
+def rate_markup(msg_id, channel_id):
     markup = InlineKeyboardMarkup()
     num_rates = 5
     markup.row_width = num_rates + 1
-    buttons = [InlineKeyboardButton("Ad", callback_data="is_ad")]
-    buttons += [InlineKeyboardButton(str(r), callback_data=f"rate_{r}") for r in range(1, num_rates+1)]
+    buttons = [InlineKeyboardButton("Ad", callback_data=f"rate_{msg_id}_{channel_id}_ad")]
+    buttons += [InlineKeyboardButton(str(r), callback_data=f"rate_{msg_id}_{channel_id}_{r}") for r in range(1, num_rates+1)]
     markup.add(*buttons)
     return markup
 
@@ -171,19 +144,21 @@ def filter_by_group_markup():
     return markup
 
 async def main():
-    msg = bot.get_message()
+    messages = bot.get_new_messages()
     try:
-        async with TelegramClient(username, api_id, api_hash) as client:
-            await client.forward_messages(chat_id, int(msg['id']), int(msg['channel_id']))
-        msg = await bot.send_message(bot.chat_id, "Rate this post", reply_markup=rate_markup())
-        bot.last_msg_id = msg.message_id
+        for message in messages:
+            async with TelegramClient(username, api_id, api_hash) as client:
+                msg_id, channel_id = int(message['id']), int(message['channel_id'])
+                await client.forward_messages(chat_id, msg_id, channel_id)
+            msg_out = await bot.send_message(bot.chat_id, "Rate this post", reply_markup=rate_markup(msg_id, channel_id))
+            bot.current_messages[(msg_id, channel_id)] = (message, msg_out)
     except MessageIdInvalidError as e:
         print(f'Got exception {e}')
-        bot.remove_message(msg)
+        # bot.remove_message(msg)
         await main()
     except ChatForwardsRestrictedError as e:
         print(f'Got exception {e}')
-        bot.remove_message(msg)
+        # bot.remove_message(msg)
         await main()
     except Exception as e:
         print(f'Got exception {e}')
@@ -195,17 +170,20 @@ async def start_message(message):
     await main()
 
 @bot.callback_query_handler(func=lambda call: True)
-async def callback_query(call):
-    if call.data == "is_ad":
-        bot.set_rating(0, True)
-        await bot.delete_message(bot.chat_id, bot.last_msg_id)
-        await bot.delete_message(bot.chat_id, bot.last_msg_id - 1)
-    elif call.data.startswith("rate_"):
-        rating = call.data.split("rate_")[-1]
-        bot.set_rating(int(rating), False)
-        await bot.delete_message(bot.chat_id, bot.last_msg_id)
-        if int(rating) < 4:
-            await bot.delete_message(bot.chat_id, bot.last_msg_id - 1)
+async def callback_query(call):    
+    if call.data.startswith("rate_"):
+        msg_id, chat_id, rating = call.data.split("_")[1:]
+        # print(f"Rating: {msg_id, chat_id, rating }")
+        # print(bot.current_messages)
+        message, msg_out = bot.current_messages.pop((int(msg_id), int(chat_id)))
+        await bot.delete_message(bot.chat_id, msg_out.message_id)
+        if rating == 'ad':
+            bot.rate(message, 0, True)
+            await bot.delete_message(bot.chat_id, msg_out.message_id - 1)
+        else:
+            bot.rate(message, int(rating), False)
+            if int(rating) < 4:
+                await bot.delete_message(bot.chat_id, msg_out.message_id - 1)
     elif call.data.startswith("group_"):
         bot.group = call.data.split("group_")[-1]
         await bot.send_message(bot.chat_id, "Send the channel name/link")
@@ -215,7 +193,9 @@ async def callback_query(call):
         bot.filter_by_group = group
         bot.filter_messages(group)
         await bot.send_message(bot.chat_id, f"Selected group {bot.filter_by_group}")
-    await main()
+    print(len(bot.current_messages))
+    if len(bot.current_messages) == 0:
+        await main()
 
 @bot.message_handler(content_types=["text"])
 async def handle_text(message):
